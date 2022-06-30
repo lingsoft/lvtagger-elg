ed25133ffcdcb09527b0a28c5e6d34f45c7a1bf3
LizaLing <107042985+LizaLing@users.noreply.github.com>
2022-06-30 12:11:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from elg import FlaskService
-from elg.model import AnnotationsResponse
+from elg.model import AnnotationsResponse, TextsResponse
 import subprocess
 import io
 import os
@@ -8,56 +8,84 @@ from os.path import exists
 class LVTagger(FlaskService):
     
     def convert_outputs(self, outputs, content, endpoint, incorrect_result):
-        annotations = {}
-        offset = 0
-        tokens = [x for x in outputs.split("\n") if x != ""]
-        for token in tokens:
-            token_split = [x for x in token.split("\t") if x != ""]
-            if endpoint == "tagger":
-                word = token_split[1]
-            elif endpoint == "ner":
-                word = token_split[0]
-            features = token_split[3:]
-            found_index = content.find(word)
-            if found_index == -1 and incorrect_result == False:
-                print("The input contains unsupported characters (probably smileys), \
-                       and the results might be incorrect.")
-                incorrect_result = True
-            start = found_index + offset
-            end = start + len(word)
-            content = content[end - offset:]
-            offset = end
-            if endpoint == "tagger":
+        if endpoint in ["word_analysis", "wordforms"]:
+            texts = []
+            lines = [x for x in outputs.split("\n") if x != ""]
+            groups = []
+            group = []
+            for line in lines:
+                if line.startswith("\t\t"):
+                    group.append(line[2:]) # this is morpho-feature
+                elif group == []:
+                    group.append(line[1:]) # this is the first tag
+                else:
+                    groups.append(group)
+                    group = [line[1:]] # this is the next tag 
+            groups.append(group)
+            for group in groups:
                 feature_dict = {}
-                tag1 = features[0]
-                feature_dict["tag1"] = tag1
-                feature_dict["tag2"] = features[1]
-                pos = tag1[0]
-                if len(features) > 2:
-                    features_split = features[2].split("|")
-                    for x in features_split:
-                        [k,v] = x.split("=")
-                        if v != "":
-                            feature_dict[k] = v
-                annot = {
-                    "start": start,
-                    "end": end,
-                    "features": feature_dict
-                    }
-                annotations.setdefault(pos, []).append(annot)
-            elif endpoint == "ner":
-                label = token_split[2]
-                if label != "O":
+                for x in group[1:]: # the first element is tag
+                    [k,v] = x.split(" = ")
+                    if v != "":
+                        feature_dict[k.replace(" ", "_")] = v
+                text = {"role":"alternative",
+                        "content": group[0], # tag
+                        "features": feature_dict}
+                texts.append(text)
+            if exists("inputfile.txt"):
+                os.remove("inputfile.txt")
+            return TextsResponse(texts = texts)
+        else:
+            annotations = {}
+            offset = 0
+            tokens = [x for x in outputs.split("\n") if x != ""]
+            for token in tokens:
+                token_split = [x for x in token.split("\t") if x != ""]
+                if endpoint == "tagger":
+                    word = token_split[1]
+                elif endpoint == "ner":
+                    word = token_split[0]
+                features = token_split[3:]
+                found_index = content.find(word)
+                if found_index == -1 and incorrect_result == False:
+                    print("The input contains unsupported characters (probably smileys), \
+                           and the results might be incorrect.")
+                    incorrect_result = True
+                start = found_index + offset
+                end = start + len(word)
+                content = content[end - offset:]
+                offset = end
+                if endpoint == "tagger":
+                    feature_dict = {}
+                    tag1 = features[0]
+                    feature_dict["tag1"] = tag1
+                    feature_dict["tag2"] = features[1]
+                    pos = tag1[0]
+                    if len(features) > 2:
+                        features_split = features[2].split("|")
+                        for x in features_split:
+                            [k,v] = x.split("=")
+                            if v != "":
+                                feature_dict[k.replace(" ", "_")] = v
                     annot = {
-                                "start": start,
-                                "end": end,
-                            }
-                    annotations.setdefault(label, []).append(annot)
-        if exists("inputfile.txt"):
-            os.remove("inputfile.txt")
-        if exists("outputfile.txt"):
-            os.remove("outputfile.txt")
-        return AnnotationsResponse(annotations = annotations)
+                        "start": start,
+                        "end": end,
+                        "features": feature_dict
+                        }
+                    annotations.setdefault(pos, []).append(annot)
+                elif endpoint == "ner":
+                    label = token_split[2]
+                    if label != "O":
+                        annot = {
+                                    "start": start,
+                                    "end": end,
+                                }
+                        annotations.setdefault(label, []).append(annot)
+            if exists("inputfile.txt"):
+                os.remove("inputfile.txt")
+            if exists("outputfile.txt"):
+                os.remove("outputfile.txt")
+            return AnnotationsResponse(annotations = annotations)
 
     def process_text(self, content):
         incorrect_result = False
@@ -77,6 +105,12 @@ class LVTagger(FlaskService):
                                   "tagger-2.2.1-jar-with-dependencies.jar" \
                                   edu.stanford.nlp.ie.crf.CRFClassifier -prop lv-ner.prop', \
                                   shell=True, stdout=subprocess.PIPE)
+        elif endpoint == "word_analysis":
+            p = subprocess.Popen('java -jar morphology-2.2.5-SNAPSHOT.jar "analysis" "' + \
+                                 content.content + '"', shell=True, stdout=subprocess.PIPE)
+        else:
+            p = subprocess.Popen('java -jar morphology-2.2.5-SNAPSHOT.jar "wordforms" "' + \
+                                 content.content + '"', shell=True, stdout=subprocess.PIPE)
         output, errors = p.communicate()
         output_utf8 = output.decode("utf-8")
         return self.convert_outputs(output_utf8, content.content, endpoint, incorrect_result)
